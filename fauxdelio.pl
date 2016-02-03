edd4872b807764f7ce0a8ccf8f91c139a401e839
carlos guillen <carlos.guillen@mtnsat.com>
2016-02-03 17:07:53 +0000

    say it

--- fauxdelio.pl
+++ fauxdelio.pl
@@ -150,7 +150,7 @@ sub buildManifest {
                 "CLM=$balance"
             );
         $record .= $US.join($US, @arr);
-	print join(',', @arr), "\n";
+	say join(',', @arr), "\n";
     }
 
     $record .= $ETX."_";
